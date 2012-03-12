9590244448f1801a400f07a15799b32ec1a40093
Joe Levy <jal50@duke.edu>
2012-03-12 16:19:41 -0400

    tiny addition on correct page

--- application/views/correct.php
+++ application/views/correct.php
@@ -6,6 +6,7 @@
 	<label for="answer">Team Name:</label>&nbsp;<input type="text" name="team" />
     <br /><br />
     <input type="hidden" name="checksum" value="<?php echo $checksum;?>" />
+    <input type="hidden" name="answer" value="<?php echo $answer;?>" />
     <input type="hidden" name="question" value="<?php echo $question;?>" />
     <input type="hidden" name="points" value="<?php echo $points;?>"  />
     <input type="submit" value="Submit" />	
